9c0508fe0e182b9ed71ea12933a7a7c3dbe527bf
IndraSyaputraS <syaput123@gmail.com>
2023-11-13 12:24:27 +0700

    Navigation

--- src/assets/Images/index.js
+++ src/assets/Images/index.js
@@ -8,7 +8,9 @@ import spoon from './spoon.png'
 import train from './train.png'
 import car from './hatchback.png'
 import fork from './cutlery.png'
+import promo1 from './promo1.jpg'
+import promo2 from './promo2.jpg'
 
 export {
-    sprite, box, bill, more, scooter, shopping, spoon, train, car, fork
+    sprite, box, bill, more, scooter, shopping, spoon, train, car, fork, promo1, promo2
 }
